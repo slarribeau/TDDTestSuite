efe74d8cb8c9aba644c1b5f5fbd181487c9a81f3
Scott Larribeau <slarribeau@gmail.com>
2018-02-09 21:40:03 -0800

    Initial commit

---
+++ src/TestSuite2.java
@@ -0,0 +1,10 @@
+
+import org.junit.Test;
+
+public class TestSuite2 {
+    @Test
+    public void doTest2() {
+        System.out.println("Test2");
+    }
+}
+
---
+++ src/mane.java
@@ -0,0 +1,8 @@
+import org.junit.runner.JUnitCore;
+
+public class mane {
+    public static void main(String[] args) {
+        System.out.println("scott");
+        JUnitCore.runClasses(TestRunner.class);
+    }
+}
